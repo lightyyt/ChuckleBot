fcaaebb3c1c4f44e4383660828875b6edb20e016
lightyyt <lightmagkuchen@gmail.com>
2024-06-27 20:55:11 +0200

    fixed reply being replay

--- actions/reboot.js
+++ actions/reboot.js
@@ -1,6 +1,6 @@
 const _reboot = (msg) => {
   if(msg.author.id==618031275961352203) {
-    msg.replay("Okay Boss!");
+    msg.reply("Okay Boss!");
     doReboot();
   } else {
     msg.reply("hmm.. no. i dont think so");
